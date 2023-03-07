586de3ab8ad57e4b2678d7039d326fd82d1db8b4
koson <567256+koson@users.noreply.github.com>
2023-03-08 00:27:58 +0700

    update

---
+++ gsl/vector/reim.c
@@ -0,0 +1,44 @@
+#include <config.h>
+#include <stdlib.h>
+#include <gsl/gsl_vector.h>
+
+#include "../../gsl/vector/view.h"
+
+#define BASE_GSL_COMPLEX_LONG
+#include "../../gsl/templates_on.h"
+#include "../../gsl/vector/reim_source.c"
+#include "../../gsl/templates_off.h"
+#undef  BASE_GSL_COMPLEX_LONG
+
+#define BASE_GSL_COMPLEX
+#include "../../gsl/templates_on.h"
+#include "../../gsl/vector/reim_source.c"
+#include "../../gsl/templates_off.h"
+#undef  BASE_GSL_COMPLEX
+
+#define BASE_GSL_COMPLEX_FLOAT
+#include "../../gsl/templates_on.h"
+#include "../../gsl/vector/reim_source.c"
+#include "../../gsl/templates_off.h"
+#undef  BASE_GSL_COMPLEX_FLOAT
+
+#define USE_QUALIFIER
+#define QUALIFIER const
+
+#define BASE_GSL_COMPLEX_LONG
+#include "../../gsl/templates_on.h"
+#include "../../gsl/vector/reim_source.c"
+#include "../../gsl/templates_off.h"
+#undef  BASE_GSL_COMPLEX_LONG
+
+#define BASE_GSL_COMPLEX
+#include "../../gsl/templates_on.h"
+#include "../../gsl/vector/reim_source.c"
+#include "../../gsl/templates_off.h"
+#undef  BASE_GSL_COMPLEX
+
+#define BASE_GSL_COMPLEX_FLOAT
+#include "../../gsl/templates_on.h"
+#include "../../gsl/vector/reim_source.c"
+#include "../../gsl/templates_off.h"
+#undef  BASE_GSL_COMPLEX_FLOAT
---
+++ gsl/vector/test_static.c
@@ -0,0 +1,7 @@
+
+#undef HAVE_INLINE
+#ifndef NO_INLINE
+#define NO_INLINE
+#endif
+#define DESC " (static)"
+#include "../../gsl/vector/test.c"
